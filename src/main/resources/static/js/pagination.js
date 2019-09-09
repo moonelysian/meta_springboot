94990639475108c25858b800164dbaf02138a7e7
moonelysian <dandyblack@naver.com>
2019-09-09 14:31:58 +0900

    pagination bar 제작

--- src/main/resources/static/js/pagination.js
+++ src/main/resources/static/js/pagination.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
-	$.ajax({
-		type: "GET",
-		url:"http://localhost:8080/api/count",
-		success: function(data){
-			alert(data)
-		},
-		error: function(error){
-			console.log(error);
-		}
+	
+	
+	var row = $("select option:selected").val();
+	makeBar(row);
+	
+	$('select').change(function(row){
+		row = $("select option:selected").val();
+		makeBar(row);
 	});
+
+	function makeBar(row){
+			$.ajax({
+			type: "GET",
+			url:"http://localhost:8080/api/count",
+			success: function(data){
+				var pageCnt = Math.ceil(data/row);
+				$('.page-item').empty();
+				for(var i=0; i<pageCnt; i++){
+					var pageNum = '#page'+i;
+					var next = i+1;
+					$(pageNum).after(
+						"<li "+ "id='page"+ next +"'"+" class=\"page-item\">"+
+						"<a class=\"page-link\" href=\"#\">"+ next +"</a>" +
+						"</li>"
+					);
+				}
+			},
+			error: function(error){
+				console.log(error);
+			}
+		});
+	}
 });

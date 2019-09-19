d8eb2003a35070db6718e29506f7c605b79b0ae0
moonelysian <dandyblack@naver.com>
2019-09-19 17:55:06 +0900

    create시 required 확인

--- src/main/resources/static/js/new.js
+++ src/main/resources/static/js/new.js
@@ -6,21 +6,36 @@ var app = new Vue({
 		},
 		methods:{
 			sendPost: function(){
-   				$.ajax({
-					method:"post",
-					url:"http://localhost:8080/api/create",
-					data: {
-						title: this.title, 
-						content: this.content
-					},
-					success: function(data){   
-						location.href="http://localhost:8080/";
-			        },
-			        error: function(err) {
-			        	console.log(err);
-			        	alert(err);
-			        }
-			  });
+				if(app.checkRequire()){
+					$.ajax({
+						method:"post",
+						url:"http://localhost:8080/api/create",
+						data: {
+							title: this.title, 
+							content: this.content
+						},
+						success: function(data){   
+							location.href="http://localhost:8080/";
+				        },
+				        error: function(err) {
+				        	console.log(err);
+				        	alert(err);
+				        }
+				  });
+				}
+		},
+		checkRequire: function(){
+			if(this.title!=''){
+				if(this.content!=''){
+					return true;
+				}
+				else{
+					alert("내용을 입력하세요!");
+				}
+			}
+			else{
+				alert("제목을 입력하세요!");
+			}
 		}
 	}
 });
